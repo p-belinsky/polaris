154755ad4fd6a6e699110c051f35c505af92127b
Phil Belinsky <philbelinsky@yahoo.com>
2026-02-06 15:53:59 -0500

    03: database setup

---
+++ src/components/providers.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import {ClerkProvider, useAuth, UserButton} from "@clerk/nextjs";
+import {ConvexProviderWithClerk} from "convex/react-clerk";
+import {Authenticated, AuthLoading, ConvexReactClient, Unauthenticated} from "convex/react";
+import {ThemeProvider} from "./theme-provider";
+import {UnauthenticatedView} from "@/features/auth/components/unauthenticated-view";
+import {AuthLoadingView} from "@/features/auth/components/auth-loading-view";
+import {dark} from "@clerk/themes";
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+export const Providers = ({children}: { children: React.ReactNode }) => {
+    return (
+        <ClerkProvider appearance={{ theme: dark }}>
+            <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+                <ThemeProvider>
+                    <Authenticated>
+                        <UserButton/>
+                        {children}
+                    </Authenticated>
+                    <Unauthenticated>
+                        <UnauthenticatedView/>
+                    </Unauthenticated>
+                    <AuthLoading>
+                        <AuthLoadingView/>
+                    </AuthLoading>
+                </ThemeProvider>
+            </ConvexProviderWithClerk>
+        </ClerkProvider>
+    )
+}
